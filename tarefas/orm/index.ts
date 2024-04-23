b9f39eb5504338ac421b4cbea2293eb8300c9a78
Tallys-Aureliano <tallysaureliano@outlook.com>
2024-04-22 23:36:10 -0300

    :sparkles:feat: Connection with database #1

--- tarefas/orm/index.ts
+++ tarefas/orm/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+const odbc = require('odbc');
+const connection = odbc.connect('Driver={PostgreSQL Unicode};Server=localhost;Port=5400;Database=empresa_db;Uid=taciano;Pwd=password;', (error: any, connection: { query: (arg0: string, arg1: (error: any, result: any) => void) => void; }) => {
+    connection.query('SELECT * FROM funcionario', (error: any, result: any) => {
+        if (error) { console.error(error) }
+        console.log(result);
+    });
+});
